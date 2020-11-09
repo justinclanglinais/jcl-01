8115b3204cd40ce86c32994db9aaf25574606576
Justin Langlinais <justinclanglinais@gmail.com>
2020-11-09 11:54:17 -0800

    feat(Header): set up component

---
+++ src/components/Header.js
@@ -0,0 +1,88 @@
+import React, {Component} from 'react';
+
+export default Header extends Component {
+    render () {
+        let resumeData = this.props.resumeData;
+        return (
+            <React.Fragment>
+                <header id='home'>
+                    <nav id='nav-wrap'>
+                        <a className='mobile-btn' href='#nav-wrap' title='Show Navigation'>
+                            Show Navigation
+                        </a>
+                        <a className='mobile-btn' href='#' title='Hide Navigation'>
+                            Hide Navigation
+                        </a>
+                        <ul id='nav' className='nav'>
+                            <li className='current'>
+                                <a className='smoothscroll' href='#home'>
+                                    Home
+                                </a>
+                            </li>
+                            <li>
+                                <a classname='smoothscroll' href='#about'>
+                                    About
+                                </a>
+                            </li>
+                            <li>
+                                <a classname='smoothscroll' href='#resume'>
+                                    Resume
+                                </a>
+                            </li>
+                            <li>
+                                <a classname='smoothscroll' href='#portfolio'>
+                                    Portfolio
+                                </a>
+                            </li>
+                            <li>
+                                <a classname='smoothscroll' href='#testimonials'>
+                                    Testimonials
+                                </a>
+                            </li>
+                            <li>
+                                <a classname='smoothscroll' href='#contact'>
+                                    Contact
+                                </a>
+                            </li>
+                            
+                        </ul>
+                    </nav>
+                    <div className='row banner'>
+                        <div className='banner-text'>
+                            <h1 className='responsive-headline'>
+                                I am {resumeData.name}.
+                            </h1>
+                            <h3 style={{
+                                color: '#fff',
+                                fontFamily: 'sans-serif'
+                            }}>
+                                I am a {resumeData.role}. {resumeData.roleDescription}
+                            </h3>
+                            <hr />
+                            <ul className='social'>
+                                {
+                                    resumeData.socialLinks && resumeData.socialLinks.map(item => {
+                                        return (
+                                            <li key={item.name}>
+                                                <a href={item.url} target=''>
+                                                    <i className={item.className}></i>
+                                                </a>
+                                            </li>
+                                        )
+                                    })
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                    <p className='scrolldown'>
+                        <a className='smoothscroll' href='#about'>
+                            <i className='icon-down-circle'>
+                                
+                            </i>
+                        </a>
+                    </p>
+                </header>
+            </React.Fragment>
+        )
+    }
+}
