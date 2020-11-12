1dc979f9bb4c21941e0d449f0a97387161ecab65
Justin Langlinais <justinclanglinais@gmail.com>
2020-11-11 20:04:08 -0800

    feat(resumeData): finish data

--- src/resumeData.js
+++ src/resumeData.js
@@ -9,17 +9,17 @@ let resumeData = {
         {
             'name' : 'LinkedIn',
             'url' : 'https://www.linkedin.com/in/justin-langlinais/',
-            'className' ; 'fa fa-linkedin'
+            'className' : 'fa fa-linkedin'
         },
         {
             'name' : 'GitHub',
             'url' : 'https://github.com/justinclanglinais',
-            'className' ; 'fa fa-github'
+            'className' : 'fa fa-github'
         },
         {
             'name' : 'Twitter',
             'url' : 'https://twitter.com/justinjitsu1',
-            'className' ; 'fa fa-twitter'
+            'className' : 'fa fa-twitter'
         }
     ],
     'aboutme' : `Full-Stack Web Developer skilled in Object-Oriented Programming, Ruby on Rails, JavaScript, React.js, SQL, Postgres, ActiveRecord, and Redux. Naturally driven to dig in and understand how things work, having taken apart and re-assembled everything mechanical that I came into contact with growing up. In 10 years spent as a Motion Graphics Designer and Video Editor, I utilized Adobe After Effects, Premiere Pro, Maya, Cinema4D, and Flash. As a leader in restaurant industry, developed People Management, Hiring & Training, and Labor Management skills. Managed teams of over 40 people across three shops, spearheading hiring, training, and team development. As a Brazilian Jiu-Jitsu practitioner, I have learned from several high-level instructors and competitors around the world. I have always been curious about coding, and loved the scripting & coding involved in motion graphics and 3D work. This year, I decided to invest in another skillset, and attended Flatiron School's Software Engineering bootcamp!`,
@@ -43,7 +43,94 @@ let resumeData = {
     ],
     'work' : [
         {
-            'CompanyName' : ''
+            'CompanyName' : 'Whole Foods Market',
+            'specialization' : 'Work Specialization?',
+            'MonthOfLeaving' : 'March',
+            'YearOfLeaving' : '2020',
+            'Achievements' : 'Work Achievements?'
+        },
+        {
+            'CompanyName' : `Molly Moon's Homemade Ice Cream`,
+            'specialization' : 'Work Specialization?',
+            'MonthOfLeaving' : 'October',
+            'YearOfLeaving' : '2018',
+            'Achievements' : 'Work Achievements?'
+        },
+        {
+            'CompanyName' : 'RUN Studios',
+            'specialization' : 'Work Specialization?',
+            'MonthOfLeaving' : 'June',
+            'YearOfLeaving' : '2015',
+            'Achievements' : 'Work Achievements?'
+        },{
+            'CompanyName' : 'cvwmedia',
+            'specialization' : 'Work Specialization?',
+            'MonthOfLeaving' : 'June',
+            'YearOfLeaving' : '2013',
+            'Achievements' : 'Work Achievements?'
         }
+    ],
+    'skillsDescription' : 'Technical Skills',
+    'skills' : [
+        {
+            'skillname' : 'Javascript'
+        },
+        {
+            'skillname' : 'React.js'
+        },
+        {
+            'skillname' : 'Ruby'
+        },
+        {
+            'skillname' : 'Ruby on Rails'
+        },
+        {
+            'skillname' : 'HTML'
+        },
+        {
+            'skillname' : 'CSS'
+        },
+        {
+            'skillname' : 'SQL'
+        },
+        {
+            'skillname' : 'Postgres'
+        },
+        {
+            'skillname' : 'Git'
+        },
+    ],
+    'portfolio' : [
+        {
+            'name' : 'Justin Jitsu',
+            'description' : `Martial Arts Class Management app for students/parents to sign up and track classes`,
+            'imgurl' : 'img.jpg'
+        },
+        {
+            'name' : 'Swolemates',
+            'description' : `Gym-Finder app to empower consumer in searching and joining gyms`,
+            'imgurl' : 'img.jpg'
+        },
+        {
+            'name' : 'MovieShelf',
+            'description' : `Movie Collection app giving you the ability to collect and organize your movies`,
+            'imgurl' : 'img.jpg'
+        }
+    ],
+    'testimonials' : [
+        {
+            'description' : `Justin is very very cool!`,
+            'name' : 'Satan'
+        },
+        {
+            'description' : `This guy really knows how to code!`,
+            'name' : 'Bill Grates -Michaelsoft'
+        },
+        {
+            'description' : `Holy potatoes!`,
+            'name' : 'Chad Kroeger'
+        },   
     ]
 }
+
+export default resumeData
