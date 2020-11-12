00f9f61f9988c333f01bf76399aacec9d4e5ef66
Justin Langlinais <justinclanglinais@gmail.com>
2020-11-11 19:08:01 -0800

    feat(resumeData): start building

---
+++ src/resumeData.js
@@ -0,0 +1,21 @@
+let resumeData = {
+    'imagebaseurl' : 'https://ivy555.github.io/',
+    'name' : 'Justin Langlinais',
+    'role' : 'Full-Stack Developer',
+    'linkedinId' : 'justin-langlinais',
+    'skypeId' : '666',
+    'roleDescription' : 'This is my description',
+    'socialLinks' : [
+        {
+            'name' : 'LinkedIn',
+            'url' : 'https://www.linkedin.com/in/justin-langlinais/',
+            'className' ; 'fa fa-linkedin'
+        },
+        {
+            'name' : 'GitHub',
+            'url' : 'https://www.linkedin.com/in/justin-langlinais/',
+            'className' ; 'fa fa-linkedin'
+        },
+        
+    ]
+}
