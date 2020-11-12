ee9bd8db487cef62848dd1a597dd9d80a924f29a
Justin Langlinais <justinclanglinais@gmail.com>
2020-11-11 19:26:41 -0800

    feat(resumeData): add more

--- src/resumeData.js
+++ src/resumeData.js
@@ -13,9 +13,37 @@ let resumeData = {
         },
         {
             'name' : 'GitHub',
-            'url' : 'https://www.linkedin.com/in/justin-langlinais/',
-            'className' ; 'fa fa-linkedin'
+            'url' : 'https://github.com/justinclanglinais',
+            'className' ; 'fa fa-github'
         },
-        
+        {
+            'name' : 'Twitter',
+            'url' : 'https://twitter.com/justinjitsu1',
+            'className' ; 'fa fa-twitter'
+        }
+    ],
+    'aboutme' : `Full-Stack Web Developer skilled in Object-Oriented Programming, Ruby on Rails, JavaScript, React.js, SQL, Postgres, ActiveRecord, and Redux. Naturally driven to dig in and understand how things work, having taken apart and re-assembled everything mechanical that I came into contact with growing up. In 10 years spent as a Motion Graphics Designer and Video Editor, I utilized Adobe After Effects, Premiere Pro, Maya, Cinema4D, and Flash. As a leader in restaurant industry, developed People Management, Hiring & Training, and Labor Management skills. Managed teams of over 40 people across three shops, spearheading hiring, training, and team development. As a Brazilian Jiu-Jitsu practitioner, I have learned from several high-level instructors and competitors around the world. I have always been curious about coding, and loved the scripting & coding involved in motion graphics and 3D work. This year, I decided to invest in another skillset, and attended Flatiron School's Software Engineering bootcamp!`,
+    'address' : 'Seattle',
+    'website' : 'https://www.justinlanglinais.com',
+    'education' : [
+        {
+            'UniversityName' : 'Flatiron School',
+            'specialization' : 'Software Engineering',
+            'MonthOfPassing' : 'August',
+            'YearOfPassing' : '2020',
+            'Achievements' : 'Some Achievements here'
+        },
+        {
+            'UniversityName' : 'Digital Media Institute at NOC',
+            'specialization' : 'Digital Media Animation & Design',
+            'MonthOfPassing' : 'June',
+            'YearOfPassing' : '2007',
+            'Achievements' : 'Some Achievements here'
+        }
+    ],
+    'work' : [
+        {
+            'CompanyName' : ''
+        }
     ]
 }
